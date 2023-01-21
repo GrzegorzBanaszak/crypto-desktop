0b2efbce8202cf174fef0dcdcf19c2649d80fb36
GrzegorzBanaszak <grzegorz.banaszak95@gmail.com>
2023-01-21 16:05:54 +0100

    Sidebar nav

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,14 +1,73 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import LogoIcon from "../icons/LogoIcon.vue";
+import SidebarLink from "./SidebarLink.vue";
+import OverviewIcon from "../icons/OverviewIcon.vue";
+import BankIcon from "../icons/BankIcon.vue";
+import WalletsIcon from "../icons/WalletsIcon.vue";
+import BundlesIcon from "../icons/BundlesIcon.vue";
+import ReportingIcon from "../icons/ReportingIcon.vue";
+import CommunityIcon from "../icons/CommunityIcon.vue";
+</script>
 
 <template>
-  <aside className="sidebar-container">main</aside>
+  <aside className="sidebar-container">
+    <h1 className="sidebar-logo"><LogoIcon /> Payte</h1>
+    <ul className="sidebar-nav">
+      <li>
+        <sidebar-link
+          :link-target="'/overview'"
+          :link-text="'Overview'"
+          :notification="9"
+        >
+          <OverviewIcon
+        /></sidebar-link>
+      </li>
+      <li>
+        <sidebar-link :link-target="'/buy-sell'" :link-text="'Buy / Sell'">
+          <BankIcon
+        /></sidebar-link>
+      </li>
+      <li>
+        <sidebar-link :link-target="'/wallets'" :link-text="'Wallets'">
+          <WalletsIcon
+        /></sidebar-link>
+      </li>
+      <li>
+        <sidebar-link :link-target="'/bundles'" :link-text="'Bundles'">
+          <BundlesIcon
+        /></sidebar-link>
+      </li>
+      <li>
+        <sidebar-link :link-target="'/reporting'" :link-text="'Reporting'">
+          <ReportingIcon
+        /></sidebar-link>
+      </li>
+      <li>
+        <sidebar-link :link-target="'/community'" :link-text="'Community'">
+          <CommunityIcon
+        /></sidebar-link>
+      </li>
+    </ul>
+  </aside>
 </template>
 
-<style scoped lang="scss">
+<style lang="scss">
 .sidebar {
   &-container {
     background-color: #f7f7f9;
     width: 15%;
+    padding: 1rem;
+  }
+
+  &-logo {
+    display: flex;
+    align-items: center;
+    font-weight: 600;
+    gap: 0.7rem;
+  }
+  &-nav {
+    list-style-type: none;
+    margin-top: 4rem;
   }
 }
 </style>
---
+++ src/components/SidebarLink.vue
@@ -0,0 +1,45 @@
+<template>
+  <router-link className="link" :to="linkTarget"
+    ><slot></slot> {{ linkText }}
+    <span v-if="notification" className="link-notification"
+      >+{{ notification }}</span
+    >
+  </router-link>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  linkTarget: { type: String, required: true },
+  linkText: { type: String, required: true },
+  notification: { type: Number },
+});
+</script>
+
+<style lang="scss">
+.link {
+  stroke: #9896a1;
+  color: #9896a1;
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+  padding: 0.5rem;
+  border-radius: 4px;
+  margin: 1.2rem 0;
+  &:hover {
+    background-color: white;
+    cursor: pointer;
+    stroke: #7445fb;
+    color: #0a041c;
+    font-weight: 600;
+  }
+  &-notification {
+    background-color: #ea4d4d;
+    color: white;
+    border-radius: 30px;
+    font-size: 10px;
+    padding: 0.2rem 0.5rem;
+    margin-left: auto;
+    font-weight: 700;
+  }
+}
+</style>
