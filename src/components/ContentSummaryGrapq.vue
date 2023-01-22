d5cb34a4d8402995127aa0328f773ea8162a262f
GrzegorzBanaszak <grzegorz.banaszak95@gmail.com>
2023-01-22 17:30:03 +0100

    Content - summery section

---
+++ src/components/ContentSummaryGrapq.vue
@@ -0,0 +1,76 @@
+<script setup lang="ts">
+import { Line } from "vue-chartjs";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  scales,
+  Filler,
+} from "chart.js";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  scales,
+  Filler
+);
+
+const chartData = {
+  labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+  datasets: [
+    {
+      borderColor: "#7445FB",
+      backgroundColor: "rgba(116,69,251,16%)",
+      borderWidth: 2,
+      data: [
+        0, 5000, 10000, 13500, 19000, 25000, 27000, 25000, 18000, 20000, 21432,
+      ],
+      tension: 0.4,
+      pointRadius: 0,
+
+      fill: true,
+    },
+    {
+      borderColor: "#9896A1",
+      borderWidth: 2,
+      data: [
+        0, 7000, 11000, 15000, 16000, 15000, 13000, 10000, 5000, 7000, 9000,
+      ],
+      tension: 0.4,
+      pointRadius: 0,
+      showLine: true,
+      fill: false,
+      borderDash: [4],
+    },
+  ],
+};
+
+const config = {
+  responsive: true,
+  maintainAspectRatio: false,
+  scales: {
+    x: {
+      display: false,
+    },
+  },
+  plugins: {
+    legend: {
+      display: false,
+    },
+  },
+};
+</script>
+
+<template><Line :data="chartData" :options="config" /></template>
+
+<style lang="scss"></style>
--- src/components/ContentSummery.vue
+++ src/components/ContentSummery.vue
@@ -1,12 +1,70 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import DotsIcon from "../icons/DotsIcon.vue";
+import ContentSummeryGrapq from "./ContentSummaryGrapq.vue";
+</script>
 
-<template><div className="main-summary">Summary</div></template>
+<template>
+  <div className="main-summary">
+    <h2 className="main-summary__header">Summary<DotsIcon /></h2>
+    <div className="main-summary__grapq">
+      <ContentSummeryGrapq />
+    </div>
+    <div className="main-summary__info">
+      <div className="main-summary__info--this"><span></span> This month</div>
+      <div className="main-summary__info--last"><span></span> Last month</div>
+    </div>
+  </div>
+</template>
 
 <style lang="scss">
 .main {
   &-summary {
     grid-area: summary;
     background-color: #f7f7f9;
+    border-radius: 16px;
+    padding: 2rem;
+    display: flex;
+    flex-direction: column;
+    &__header {
+      display: flex;
+      align-items: center;
+      font-weight: 600;
+      font-size: 22px;
+      justify-content: space-between;
+      svg {
+        cursor: pointer;
+      }
+    }
+    &__grapq {
+      margin-top: 1rem;
+      height: 250px;
+      position: relative;
+    }
+    &__info {
+      margin-top: 0.3rem;
+      border-top: 1px solid #d5d5d6;
+      padding-top: 1.3rem;
+      display: flex;
+      color: #9896a1;
+      gap: 1rem;
+      span {
+        width: 0.5rem;
+        height: 0.5rem;
+        border-radius: 50%;
+        display: inline-block;
+        margin-right: 0.4rem;
+      }
+      &--this {
+        span {
+          background-color: #7445fb;
+        }
+      }
+      &--last {
+        span {
+          background-color: #d5d5d6;
+        }
+      }
+    }
   }
 }
 </style>
